4cbdc4672290af28a417101c114e655db00e3533
kendra <kendra@kendralee.com>
2026-05-05 16:25:48 -0400

    Stateful Widgets Complete

--- lib/main.dart
+++ lib/main.dart
@@ -57,10 +57,13 @@ class Tile extends StatelessWidget {
 }
 
 
-class GamePage extends StatelessWidget {
+class GamePage extends StatefulWidget {
   GamePage({super.key});
 
-  // This manages game logic, and is out of scope for this lesson.
+  @override
+  State<GamePage> createState() => _GamePageState();
+}
+class _GamePageState extends State<GamePage> {
   final Game _game = Game();
 
   @override
@@ -68,19 +71,23 @@ class GamePage extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.all(8.0),
       child: Column(
-        spacing: 5.0,
         children: [
-          for (final guess in _game.guesses)
+          for (var guess in _game.guesses)
             Row(
-              spacing: 5.0,
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                for (final letter in guess) Tile(letter.char, letter.type),
+                for (var letter in guess)
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 2.5, vertical: 2.5),
+                    child: Tile(letter.char, letter.type),
+                  )
               ],
             ),
           GuessInput(
-            onSubmitGuess: (guess) {
-
-              print(guess);
+            onSubmitGuess: (String guess) {
+              setState(() { //New
+                _game.guess(guess);
+               });
             },
           ),
         ],
